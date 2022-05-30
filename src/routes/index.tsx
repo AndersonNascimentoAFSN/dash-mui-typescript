004f0d8e41718ec902193f9e3968bffdde92d657
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-05-30 20:45:44 -0300

    add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../pages/index";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}

47b8e0969427e2161c4367e69059268da1193337
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-05-31 20:20:45 -0300

    material ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "@mui/material";
 import { App } from "./App";
-import { LightTheme } from "./shared/theme";
+import { AppThemeProvider } from "./shared/contexts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider theme={LightTheme}>
+      <AppThemeProvider>
         <App />
-      </ThemeProvider>
+      </AppThemeProvider>
     </Router>
   </React.StrictMode>
 );
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,54 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme, DarkTheme } from "../theme";
+import { Box } from "@mui/system";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+type themeName = "light" | "dark";
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+export const AppThemeProvider = ({ children }: AppProviderProps) => {
+  const [themeName, setThemeName] = useState<themeName>("light");
+
+  const toggleTheme = useCallback(() => {
+    setThemeName((prevState) => (prevState === "light" ? "dark" : "light"));
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === "light") return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100v"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
